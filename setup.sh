2b05ea4cff07930ae9d324d92bb0d954a8528d6d
Ashish Kumar <91678316+Ashsky72@users.noreply.github.com>
2022-03-02 20:04:43 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
+
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"your-email@domain.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+
+echo "[theme]
+primaryColor = ‘#f3c720’
+backgroundColor = ‘#1a1b1a’
+secondaryBackgroundColor = ‘#f3c720’
+textColor= ‘#f5f5f5’
+font = ‘sans serif’
+[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
